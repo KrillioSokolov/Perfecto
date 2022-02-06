959427aaa27d26ba82691980762091d4ef78b88f
Kyrylo Sokolov <sokolov.kirill22@gmail.com>
2022-02-06 19:55:37 +0200

    Initial Commit

---
+++ Ability/AbilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbilityApp.swift
+//  Ability
+//
+//  Created by Kyrylo Sokolov on 06.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct AbilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AbilityUITests/AbilityUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AbilityUITestsLaunchTests.swift
+//  AbilityUITests
+//
+//  Created by Kyrylo Sokolov on 06.02.2022.
+//
+
+import XCTest
+
+class AbilityUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
